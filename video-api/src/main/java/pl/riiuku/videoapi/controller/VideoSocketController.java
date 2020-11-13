5316a9e5d7455ba3b2814bf00e3c1db2f63c8abf
Lukasz <wegrzyniak.lukasz@gmail.com>
2020-11-13 16:55:22 +0100

    Websocket configure for string

--- video-api/src/main/java/pl/riiuku/videoapi/controller/VideoSocketController.java
+++ video-api/src/main/java/pl/riiuku/videoapi/controller/VideoSocketController.java
@@ -1,5 +1,17 @@
 package pl.riiuku.videoapi.controller;
 
 
+import org.springframework.messaging.handler.annotation.MessageMapping;
+import org.springframework.messaging.handler.annotation.SendTo;
+import org.springframework.stereotype.Controller;
+
+@Controller
 public class VideoSocketController {
+
+    @MessageMapping("/video-call")
+    @SendTo("/topic/video-call")
+    public String sendVideoBytes(String message) {
+        return message + ", String test";
+    }
+
 }
--- video-api/src/test/java/pl/riiuku/videoapi/controller/VideoSocketControllerTest.java
+++ video-api/src/test/java/pl/riiuku/videoapi/controller/VideoSocketControllerTest.java
@@ -5,6 +5,8 @@ import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.web.server.LocalServerPort;
 import org.springframework.messaging.converter.ByteArrayMessageConverter;
+import org.springframework.messaging.converter.StringMessageConverter;
+import org.springframework.messaging.simp.stomp.StompFrameHandler;
 import org.springframework.messaging.simp.stomp.StompHeaders;
 import org.springframework.messaging.simp.stomp.StompSession;
 import org.springframework.messaging.simp.stomp.StompSessionHandlerAdapter;
@@ -17,9 +19,9 @@ import java.lang.reflect.Type;
 import java.util.Arrays;
 import java.util.Collections;
 import java.util.List;
-import java.util.concurrent.ExecutionException;
-import java.util.concurrent.TimeUnit;
-import java.util.concurrent.TimeoutException;
+import java.util.concurrent.*;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 class VideoSocketControllerTest {
@@ -35,14 +37,31 @@ class VideoSocketControllerTest {
     private int port;
 
     @Test
-    public void testConnection() throws InterruptedException, ExecutionException, TimeoutException {
-        client.setMessageConverter(new ByteArrayMessageConverter());
+    public void sendVideoBytes() throws InterruptedException, ExecutionException, TimeoutException {
+
+        BlockingQueue<String> values = new ArrayBlockingQueue<>(1);
+
+        client.setMessageConverter(new StringMessageConverter());
         StompSession stomp = client
-                .connect("ws://localhost:" + port + "/socket", new StompSessionHandlerAdapter() {})
+                .connect("ws://localhost:" + port + "/socket", new StompSessionHandlerAdapter() {
+                })
                 .get(1L, TimeUnit.SECONDS);
 
 
+        stomp.subscribe("/topic/video-call", new StompFrameHandler() {
+            @Override
+            public Type getPayloadType(StompHeaders headers) {
+                return String.class;
+            }
 
+            @Override
+            public void handleFrame(StompHeaders headers, Object payload) {
+                values.add((String) payload);
+            }
+        });
 
+        stomp.send("/app/video-call", "Hello");
+
+        assertEquals("Hello, String test", values.poll(2, TimeUnit.SECONDS));
     }
 }
