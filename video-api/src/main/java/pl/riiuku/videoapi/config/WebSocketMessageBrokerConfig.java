249a1ede27a31ec3c57ab037a6c6cca57d514370
Lukasz <wegrzyniak.lukasz@gmail.com>
2020-11-21 17:20:01 +0100

    reactjs init project

--- video-api/src/main/java/pl/riiuku/videoapi/config/WebSocketMessageBrokerConfig.java
+++ video-api/src/main/java/pl/riiuku/videoapi/config/WebSocketMessageBrokerConfig.java
@@ -3,22 +3,11 @@ package pl.riiuku.videoapi.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.http.server.ServerHttpRequest;
-import org.springframework.http.server.ServerHttpResponse;
-import org.springframework.messaging.converter.MessageConverter;
 import org.springframework.messaging.simp.config.MessageBrokerRegistry;
-import org.springframework.web.socket.WebSocketHandler;
 import org.springframework.web.socket.config.annotation.EnableWebSocketMessageBroker;
 import org.springframework.web.socket.config.annotation.StompEndpointRegistry;
 import org.springframework.web.socket.config.annotation.WebSocketMessageBrokerConfigurer;
-import org.springframework.web.socket.server.HandshakeInterceptor;
 import org.springframework.web.socket.server.standard.ServletServerContainerFactoryBean;
-import org.springframework.web.socket.server.support.DefaultHandshakeHandler;
-import pl.riiuku.videoapi.repository.RoomRepository;
-import pl.riiuku.videoapi.service.validator.ValidatorService;
-
-import java.util.List;
-import java.util.Map;
 
 @EnableWebSocketMessageBroker
 @Configuration
