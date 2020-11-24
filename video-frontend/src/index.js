5c40e5ef41ba7d6e4facacb612f85c67d97ac08a
Lukasz <wegrzyniak.lukasz@gmail.com>
2020-11-24 20:00:06 +0100

    Simple router

--- video-frontend/src/index.js
+++ video-frontend/src/index.js
@@ -1,13 +1,20 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
-import Login from './login'
 import './index.css'
+import { BrowserRouter as Router, Route} from "react-router-dom";
+import {Login} from "./login";
+import {Main} from "./main";
 function Start() {
     return (
         <section>
-            <Login />
+            <Router>
+                <Route exact path="/" component={Login} />
+                <Route exact path="/main" component={Main} />
+            </Router>
         </section>
     );
 }
+
+
 
 ReactDOM.render(<Start/>, document.getElementById('root'));
--- video-frontend/src/login.js
+++ video-frontend/src/login.js
@@ -1,28 +1,29 @@
 import React from "react";
 import './login.css';
+import {useHistory} from 'react-router-dom';
 
-class Login extends React.Component {
+export function Login() {
 
+    const history = useHistory();
 
-    handleSubmit(event) {
-        event.preventDefault();
-        console.log(event)
+    function handleSubmit(props) {
+
+        history.push("/main")
+        props.preventDefault();
+        console.log(props)
     }
 
-    render() {
-        return (
-            <section className="login_form_section">
-                    <form onSubmit={this.handleSubmit}>
-                        <h1 className="login_logo">VideoApp</h1>
-                        <div className="line-break"/>
-                        <input className="login_form_input--text" placeholder="Podaj swój nick..." type="text"/>
-                        <div className="line-break"/>
-                        <input className="login_form_input--button" type="submit" value="Wyślij"/>
-                    </form>
+    return (
+        <section className="login_form_section">
+            <form onSubmit={handleSubmit}>
+                <h1 className="login_logo">VideoApp</h1>
+                <div className="line-break"/>
+                <input className="login_form_input--text" placeholder="Podaj swój nick..." type="text"/>
+                <div className="line-break"/>
+                <input className="login_form_input--button" type="submit" value="Wyślij"/>
+            </form>
 
-            </section>
-        )
-    }
+        </section>
+    )
 }
 
-export default Login
