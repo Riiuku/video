03cf804ec2c9e09f2b8cad9fe24e45ce0bd3a54e
Lukasz <wegrzyniak.lukasz@gmail.com>
2020-11-25 17:39:57 +0100

    footer

--- video-frontend/src/index.js
+++ video-frontend/src/index.js
@@ -4,13 +4,20 @@ import './index.css'
 import { BrowserRouter as Router, Route} from "react-router-dom";
 import {Login} from "./login";
 import {Main} from "./main";
+import {Footer} from "./footer";
 function Start() {
     return (
-        <section>
-            <Router>
-                <Route exact path="/" component={Login} />
-                <Route exact path="/main" component={Main} />
-            </Router>
+        <section className="index__section">
+            <article className="index__router">
+                <Router >
+                    <Route exact path="/" component={Login} />
+                    <Route exact path="/main" component={Main} />
+                </Router>
+            </article>
+            <article className="index_footer">
+                <Footer />
+            </article>
+
         </section>
     );
 }
